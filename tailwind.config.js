c14578e5388d6270beb5411030569e178e61850c
Jerry Applegarth <applegarth1630@gmail.com>
2023-02-13 08:35:49 -0800

    firt commit

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.{html,js}"],
+  theme: {
+    // screens: {
+    //   sm: "576px",
+    //   md: "768px",
+    //   lg: "992px",
+    //   xl: "1200px",
+
+    // },
+    // container: {
+    //   center: true,
+    //   padding: "1rem", // 16px
+
+    // },
+
+    // extend: {
+    //   colors: {
+    //     primary: '#fd3d57'
+    //     },
+      fontFamily : {
+        'poppins' : ['Poppins'],
+        'roboto' : ['Roboto'],
+      }
+    },
+
+ 
+  plugins: [],
+}
+
+
